e13e94efbcf6171c067e9c197a3ede731e5e1519
samihazaman <samihaz2009@gmail.com>
2024-03-28 08:42:07 -0400

    Added descriptions

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 $( document ).ready(function() {
 
-var hufflepuff = "https://www.pngkey.com/png/full/43-439176_51-hufflepuff-crest-transparent-hufflepuff-house.png";
+//var hufflepuff = "https://www.pngkey.com/png/full/43-439176_51-hufflepuff-crest-transparent-hufflepuff-house.png";
+var hufflepuff = "https://www.nicepng.com/png/full/43-439104_hufflepuff-crest-harry-potter-banner-harry-potter-hufflepuff.png"
 
-var gryffindor = "https://static.wikia.nocookie.net/harryalbuspotter/images/8/8e/0.31_Gryffindor_Crest_Transparent.png/revision/latest/scale-to-width-down/340?cb=20181022144149";
+//var gryffindor = "https://static.wikia.nocookie.net/harryalbuspotter/images/8/8e/0.31_Gryffindor_Crest_Transparent.png/revision/latest/scale-to-width-down/340?cb=20181022144149";
+var gryffindor = "https://www.nicepng.com/png/full/433-4334429_gryffindor-crest-png.png";
 
-var ravenclaw = "https://static.wikia.nocookie.net/pottermore/images/4/40/Ravenclaw_Crest_1.png/revision/latest?cb=20140604194505";
+//var ravenclaw = "https://static.wikia.nocookie.net/pottermore/images/4/40/Ravenclaw_Crest_1.png/revision/latest?cb=20140604194505";
+var ravenclaw = "https://www.nicepng.com/png/full/439-4398613_ravenclaw-harry-potter-ravenclaw-house-crest.png"
 
-var slytherin = "https://static.wikia.nocookie.net/harrypotter/images/0/00/Slytherin_ClearBG.png/revision/latest/scale-to-width-down/871?cb=20161020182557";
+//var slytherin = "https://static.wikia.nocookie.net/harrypotter/images/0/00/Slytherin_ClearBG.png/revision/latest/scale-to-width-down/871?cb=20161020182557";
+var slytherin = "https://www.nicepng.com/png/full/226-2269054_harry-potter-slytherin-crest-juniors-v-neck-t.png"
 
 var counter = 0; 
 
@@ -141,6 +145,9 @@ else {
 $(".reset").click(function() {
        $(".start").show();
        $(".container").hide();
+       $(".result").empty(); 
+       $(".image").empty(); 
+
 });
 
 });
